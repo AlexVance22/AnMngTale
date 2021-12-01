b84ada54fe30fbaa490867771dea88786d0f13d2
Alexander Vance <ayv200@gmail.com>
2021-12-01 14:42:18 +0100

    Improved consistency of assert types

--- AnMngTale/src/core/Asserts.h
+++ AnMngTale/src/core/Asserts.h
@@ -4,7 +4,7 @@
 #ifdef MNG_DEBUG
 
 	#define MNG_ASSERT_BASIC(condition) assert(condition)
-	#define MNG_ASSERT_MSG(condition, message) if (!(condition)) { std::cerr << message << '\n'; abort(); }
+	#define MNG_ASSERT_MSG(condition, message) if (!(condition)) { std::cerr << message << '\n'; assert(false); }
 	#define MNG_ASSERT_SLIM(condition) assert(condition)
 
 #else
